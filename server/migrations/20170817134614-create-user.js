3acf5a35c5592a6f73dcd10d4d85f8525f58c079
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2017-09-18 14:32:33 +0100

    Remmove commented out sequelize

--- server/migrations/20170817134614-create-user.js
+++ server/migrations/20170817134614-create-user.js
@@ -46,5 +46,5 @@ module.exports = {
         defaultValue: Sequelize.NOW
       }
     }),
-  down: (queryInterface /* , Sequelize */) => queryInterface.dropTable('Users')
+  down: (queryInterface) => queryInterface.dropTable('Users')
 };
