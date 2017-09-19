4874f506043ff74cde11ca9ab48c71312d178ec5
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2017-09-19 19:37:10 +0100

    update routes

--- server/routes/rating-routes.js
+++ server/routes/rating-routes.js
@@ -1,7 +1,7 @@
 const rateController = require('../../server/controllers/rating')
 
 module.exports = (app) => {
-  app.post('/api/:userId/:mealId/ratings', rateController.rateMeal);
+  app.post('/api/users/:userId/meals/:mealId/ratings', rateController.rateMeal);
   // app.get('/api/:userId/:mealId/ratings/:rateId', rateController.getRate);
   // app.put('/api/:userId/:mealId/ratings/:rateId', rateController.updateRate);
   // app.delete('/api/:userId/:mealId/ratings/:rateId', rateController.destroy);
