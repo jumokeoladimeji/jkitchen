664f457632f1b921fa79cfb344e05ba582882cce
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2017-09-19 14:59:50 +0100

    Update meals

--- server/controllers/meal.js
+++ server/controllers/meal.js
@@ -1,18 +1,20 @@
 const Meal = require('../models').Meal;
+const redis = require('redis');
+const client = redis.createClient();
 const Rating = require('../models').Rating;
 const Comment = require('../models').Comment;
 const MealOrderDetail = require('../models').MealOrderDetail
 
 module.exports = {
   // Only admin can create and update meal
-  create(req, res){
+  create (req, res) {
     Meal
       .create({
         title: req.body.title,
         price: req.body.price,
         available_quantity: req.body.available_quantity,
         image: req.body.image,
-        description: req.body.description,
+        description: req.body.description
       })
       .then((meal) => res.status(200).send(meal))
       .catch((error) => {
@@ -20,44 +22,50 @@ module.exports = {
       });
   },
 
-  list(req, res){
+  list (req, res) {
     Meal
       .findAll({
-      include: [
-        {
-          model: Rating,
-           as: 'ratings'
-        }
-      ]})
+        include: [
+          {
+            model: Rating,
+            as: 'ratings'
+          }
+        ]})
       .then((meals) => res.status(200).send(meals))
       .catch((error) => {
         res.status(500).send(error)
       });
   },
 
-  getOne(req, res){
-   mealId = req.params.mealId
+  getOne (req, res) {
+    const mealId = req.params.mealId
     // get meal from redis cache
-    client.get(`meal${mealId}`, function(err, reply) {
-      if(reply){
+    client.get(`meal${mealId}`, function (err, reply) {
+      if (err) {
+        return res.status(500).send(err)
+      }
+      if (reply) {
         let meal = JSON.parse(reply)
         // create a set in redis for popular meals
         // when count is more than one, add the meal to popular meals
         if (meal.count > 1) {
           client.sadd('mostPopularMeals', reply)
-        }
-        else{
+        } else {
           meal.count++
+          client.set(`meal${mealId}`, JSON.stringify(meal), function(err, reply) {
+            if (err) {
+              return res.status(500).send(err)
+            }
+            return res.status(200).send(meal);
+          });
         }
-        return res.status(200).send(meal)
-      }
-      else{
+      } else {
         return Meal
           .findById(req.params.mealId, {
             include: [{
               model: Rating,
-              as: 'ratings',
-            },{
+              as: 'ratings'
+            }, {
               model: Comment,
               as: 'comments'
             }, {
@@ -68,40 +76,37 @@ module.exports = {
           .then(meal => {
             if (!meal) {
               return res.status(404).send({
-                message: 'Meal Not Found',
+                message: 'Meal Not Found'
               });
             }
             let mealToCache = meal.dataValues
             mealToCache.count = 0
-            client.set(`meal${mealId}`, JSON.stringify(mealToCache), function(err, reply) {
+            client.set(`meal${mealId}`, JSON.stringify(mealToCache), function (err, reply) {
               return res.status(200).send(meal);
             });
           })
-          .catch((error) =>{
+          .catch((error) => {
             res.status(500).send(error)
           });
-
       }
     });
-
   },
-  getMostPopularMeals(req, res){
-
-    client.smembers('mostPopularMeals', function(err, reply){
-      if(err){
+  getMostPopularMeals (req, res) {
+    client.smembers('mostPopularMeals', function (err, reply){
+      if (err) {
         return res.status(500).send(err)
-      } 
+      }
       return res.status(200).send(reply);
     });
-
   },
-  update(req, res){
+  update (req, res) {
+    const mealId = req.params.mealId
     Meal
       .findById(req.params.mealId, {
         include: [{
           model: Rating,
-          as: 'ratings',
-        },{
+          as: 'ratings'
+        }, {
           model: Comment,
           as: 'comments'
         }, {
@@ -112,10 +117,9 @@ module.exports = {
       .then(meal => {
         if (!meal) {
           return res.status(404).send({
-            message: 'Meal Not Found',
+            message: 'Meal Not Found'
           });
-        }
-        else{
+        } else {
           return meal
             .update({
               title: req.body.title || meal.title,
@@ -125,32 +129,42 @@ module.exports = {
               description: req.body.description || meal.description,
             })
             .then((updatedMeal) => {
-              mealToUpdate = JSON.stringify(meal)
+              const mealToUpdate = JSON.stringify(meal)
               // delete meal from the mostPopularMeals set
               client.srem('mostPopularMeals', mealToUpdate)
               // add updated meal to the set
               client.sadd('mostPopularMeals', JSON.stringify(updatedMeal))
-              res.status(200).send(updatedMeal)
+              client.smembers('mostPopularMeals')
+              // update meal in redis string
+              client.set(`meal${mealId}`, JSON.stringify(updatedMeal), function(err, updatedMealInCache) {
+                return res.status(200).send(updatedMeal);
+              });
             })
         }
       })
-      .catch((error) =>{
+      .catch((error) => {
         res.status(500).send(error)
       });
   },
 
-  destroy(req, res){
+  destroy (req, res) {
+    const mealId = req.params.mealId
     Meal
       .findById(req.params.mealId)
       .then(meal => {
         if (!meal) {
           return res.status(500).send({
-            message: 'Meal Not Found',
+            message: 'Meal Not Found'
           });
         }
         return meal
           .destroy()
-          .then(() => res.status(200).send({message: 'Meal deleted.'}))
+          .then(() => {
+            const mealToDelete = JSON.stringify(meal)
+            client.srem('mostPopularMeals', mealToDelete)
+            client.del(`meal${mealId}`)
+            res.status(200).send({message: 'Meal deleted.'})
+          })
       })
       .catch((error) => res.status(500).send(error));
   }
--- server/routes/meal-routes.js
+++ server/routes/meal-routes.js
@@ -4,12 +4,10 @@ module.exports = (app) => {
   app.route('/api/meals') 
     .post(mealController.create)
     .get(mealController.list);
-  app.route('/api/meals/:mealId') 
+  app.route('/api/meals/:mealId')
     .get(mealController.getOne)
     .put(mealController.update)
     .delete(mealController.destroy);
   app.route('/api/popularMeals')
     .get(mealController.getMostPopularMeals);
 }
-
-
