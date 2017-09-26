af61c6fe786627e4ab31b91122a4628bc2c68b2e
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2017-09-26 08:09:46 +0100

    Update test set up

--- tests/public/user-controller.spec.js
+++ tests/public/user-controller.spec.js
@@ -1,6 +1,6 @@
 describe('UserCtlr', function(){
-  var controller, scope, $httpbackend;
-  beforeEach(module('Jkitchen'));
+  let controller, scope, $httpbackend;
+  beforeEach(angular.mock.module('Jkitchen'));
   beforeEach(inject(function($controller, $rootScope, $injector){
     scope = $rootScope.$new();
     $httpBackend = $injector.get('$httpBackend');
